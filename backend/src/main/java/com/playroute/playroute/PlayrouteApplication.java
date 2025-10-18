7517993c6e8378bdf87785bb18eded1a4f182a1d
nori <nori@vbminigame.sakura.ne.jp>
2025-10-18 13:00:16 +0900

    新規作成

---
+++ backend/src/main/java/com/playroute/playroute/PlayrouteApplication.java
@@ -0,0 +1,13 @@
+package com.playroute.playroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayrouteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/playroute/playroute/PlayrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playroute.playroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
